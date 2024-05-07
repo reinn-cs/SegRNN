21eb2d5cc6f13366ee2e0d8e200170d9a576d379
Reinhard <reinn.cs@gmail.com>
2024-05-07 18:57:02 +0200

    Add scripts for running alternatives

--- models/Linear.py
+++ models/Linear.py
@@ -12,10 +12,15 @@ class Model(nn.Module):
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
         self.Linear = nn.Linear(self.seq_len, self.pred_len)
+        self.activation = None
+        if configs.rnn_type == 'tanh':
+            self.activation = nn.Tanh()
         # Use this line if you want to visualize the weights
         # self.Linear.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
 
     def forward(self, x):
         # x: [Batch, Input length, Channel]
         x = self.Linear(x.permute(0,2,1)).permute(0,2,1)
+        if self.activation is not None:
+            x = self.activation(x)
         return x # [Batch, Output length, Channel]
